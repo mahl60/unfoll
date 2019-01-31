c05ca867ba579b288f6e5e8cff5f8f411db3baf2
mahl60 <43241340+mahl60@users.noreply.github.com>
2019-01-31 16:21:58 +0700

    Update bot1.php

--- bot1.php
+++ bot1.php
@@ -6,8 +6,7 @@ session_start();
 require_once('fungsi.php');
 $TimeZone="+7";
 $_time=gmdate("H", time() + ($TimeZone * 60 * 60));
-$_SESSION['data'] = array('cookies' => 'ds_user=jual_igfollow01;shbid=18600;shbts=1544840055.5197465;rur=FRC;mid=XBRjdgABAAH9OufNLXDKdw5KrE3H;ds_user_id=8429117661;urlgen="{\"36.80.219.45\": 17974}:1gXzT5:8ofH35E4CrfRVK5mNEWZmpgAPO8";sessionid=IGSC9fb950d528ec707c806ce0154bbb560bfa763aadfd15ce2a3a2f858c0976ea4f%3AEE6o1RmeXylmHHXIUNUm4baaFjKtBD7H%3A%7B%22_auth_user_id%22%3A8429117661%2C%22_auth_user_backend%22%3A%22accounts.backends.CaseInsensitiveModelBackend%22%2C%22_auth_user_hash%22%3A%22%22%2C%22_platform%22%3A1%2C%22_token_ver%22%3A2%2C%22_token%22%3A%228429117661%3A8KSPAKFuykNJZvO8ExcpStm9DCaU3MUA%3Ae2663b637973d2c0496666e9380784679f4b91fb20d606fde575d5132a5e2f97%22%2C%22last_refreshed%22%3A1544840055.5206596851%7D;mcd=3;csrftoken=T9flbbacMyU5eaiIxMTv0lYganbdzLiZ;', 'useragent' => 'Instagram 6.22.0 Android (10/3.4.4; 120; 480x320; samsung; GT-I9220; GT-I9220; smdkc210; en_US)', 'device_id' => 'android-8ce87bdda85cd44f14de9afb864918849', 'username' => 'jual_igfollow01', 'id' => '8429117661');
-
+$_SESSION['data'] = array('cookies' => 'ds_user=jual_igfollow01;shbid=18600;shbts=1548920846.1096637;rur=PRN;mid=XFKoDQABAAFNHcAMUTtL2nUylFnZ;ds_user_id=8429117661;urlgen="{\"36.84.0.24\": 17974}:1gp74I:3-26Wo7uyynRtIgwnyQLa6YQ9r0";sessionid=8429117661%3AX8tfgbyS7VaiPv%3A29;csrftoken=rC9gFd5VvjVX5mlPOYQUj2NnISgVXWcT;', 'useragent' => 'Instagram 6.22.0 Android (11/2.5.5; 240; 1080x1920; samsung; GT-N7000; GT-N7000; smdkc210; en_US)', 'device_id' => 'android-358c850b3836ae02b1d8b319d86d435f2', 'username' => 'jual_igfollow01', 'id' => '8429117661');
 $xx = 0;
 while(true){
 	if($_time>24){

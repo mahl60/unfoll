a80b17626bb7ab254b2f7c19c4c50673859f7db9
mahl60 <43241340+mahl60@users.noreply.github.com>
2019-02-09 20:14:24 +0700

    Update bot8.php

--- bot8.php
+++ bot8.php
@@ -1,5 +1,4 @@
 <?php
-header("Refresh:300");
 set_time_limit(100);
 error_reporting(0);
 ignore_user_abort(1);
@@ -8,10 +7,11 @@ require_once('fungsi.php');
 $TimeZone="+7";
 $_time=gmdate("H", time() + ($TimeZone * 60 * 60));
 $_SESSION['data'] = array('cookies' => 'ds_user=jual_igfollow08;shbid=18600;shbts=1544841265.2347393;rur=FRC;mid=XBRoMAABAAGzwKKJaB2I4YrdxegY;ds_user_id=8703009551;urlgen="{\"36.80.219.45\": 17974}:1gXzmb:i0jRQDUfvRvSLAd87lOQ1myooFQ";sessionid=IGSCafdeed987c1fd27a0e6eb288f4cdb12e2903c2944594f481ea8d688d9c8abd17%3AI2AoT3rqGCINqR43b1VqDIA7aVGWfwSd%3A%7B%22_auth_user_id%22%3A8703009551%2C%22_auth_user_backend%22%3A%22accounts.backends.CaseInsensitiveModelBackend%22%2C%22_auth_user_hash%22%3A%22%22%2C%22_platform%22%3A1%2C%22_token_ver%22%3A2%2C%22_token%22%3A%228703009551%3AdZNe87anNZWHt25hYxNUfT5GPeOctikP%3A88ff33913e6ef5c0dc29d0c5e09181cf7b197dd7e46681d5cb2fc823eb1f17e0%22%2C%22last_refreshed%22%3A1544841265.2354917526%7D;mcd=3;csrftoken=c3sd67XjEPM6GCjA1cG8PN2ugnpxOVsu;', 'useragent' => 'Instagram 6.22.0 Android (11/3.3.0; 320; 1280x720; samsung; GT-N7000; GT-N7000; smdkc210; en_US)', 'device_id' => 'android-c1aeb6517a1c7f33514f7ff69047e74e3', 'username' => 'jual_igfollow08', 'id' => '8703009551');
-if($_time>23){
-      $jumlah= "30";
+while(true){
+	if($_time>23){
+      $jumlah= "8";
         $_POST['tipe'] = "followers";
-        $target = "522969993";
+        $target = "1554192";
 	$data_session = $_SESSION['data'];
 	$getinfo = proccess(1, $data_session['useragent'], 'users/'.$target.'/info/');
 	$getinfo = json_decode($getinfo[1]);
@@ -47,7 +47,7 @@ if($_time>23){
 	for($i=0;$i<count($listids);$i++):
 			$cross = proccess(1, $data_session['useragent'], 'friendships/create/'.$listids[$i].'/', $data_session['cookies'], hook('{"user_id":"'.$listids[$i].'"}'));
 			$cross = json_decode($cross[1]);
-			print $i.'. <b>@'.$data_session['username'].'</b> <font color="green">Sukses Follow => </font><b style="color:gray;">[ @'.$listids[$i].' ]</b><br>';
+			print $i.'. <b>@'.$data_session['username'].' Follow => '.$listids[$i].PHP_EOL;
 			flush();
 	endfor;
 }
@@ -94,6 +94,9 @@ else
 			print $i.'. <b>@'.$data_session['username'].'</b> <font color="green">Sukses Follow => </font><b style="color:gray;">[ @'.$listids[$i].' ]</b><br>';
 			flush();
 	endfor;
-	 sleep(240);
+}
+	
+	sleep(250);
+	
 }
 ?>

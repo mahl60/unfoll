ffa6cde9bb0678c29c81c299f0c99c89c2310d70
mahl60 <43241340+mahl60@users.noreply.github.com>
2019-02-09 17:59:19 +0700

    Update bot5.php

--- bot5.php
+++ bot5.php
@@ -1,4 +1,5 @@
 <?php
+header("Refresh:300");
 set_time_limit(100);
 error_reporting(0);
 ignore_user_abort(1);
@@ -7,15 +8,12 @@ require_once('fungsi.php');
 $TimeZone="+7";
 $_time=gmdate("H", time() + ($TimeZone * 60 * 60));
 $_SESSION['data'] = array('cookies' => 'ds_user=jual_igfollow05;shbid=18600;shbts=1544840951.5613022;rur=FRC;mid=XBRm9gABAAHQbcqSgFWlxn7NtjCs;ds_user_id=8572058467;urlgen="{\"36.80.219.45\": 17974}:1gXzhX:BGrx9gd-pbI_68oMTSRsRCvoLCs";sessionid=IGSCb4cca011a13696b250f9e7d720a7141daa94b9fa24bc5c98dbbc7750b405d2b8%3AuVmxNs4ah0n2DQoA9GsV3pyIAMNW86cc%3A%7B%22_auth_user_id%22%3A8572058467%2C%22_auth_user_backend%22%3A%22accounts.backends.CaseInsensitiveModelBackend%22%2C%22_auth_user_hash%22%3A%22%22%2C%22_platform%22%3A1%2C%22_token_ver%22%3A2%2C%22_token%22%3A%228572058467%3AqqONG0OSuMC8VyeaXwbnqaU6aknkqK9R%3A4df7905e60aef693a99bf19d05b984479fdffb6d12039d640ac9d89fab7153ad%22%2C%22last_refreshed%22%3A1544840951.5620718002%7D;mcd=3;csrftoken=rIrwR0hJnqAySfZPRg39GD6Ijvfn9ZoE;', 'useragent' => 'Instagram 6.22.0 Android (11/2.4.3; 320; 768x1024; samsung; SM-N9000; SM-N9000; smdkc210; en_US)', 'device_id' => 'android-64b327d40918ca7522ac22e3d02dc41c3', 'username' => 'jual_igfollow05', 'id' => '8572058467');
-
-$xx = 0;
-while(true){
-	if($_time>24){
-      $jumlah= "9";
+if($_time>23){
+      $jumlah= "30";
         $_POST['tipe'] = "followers";
-        $target = "5451786";
+        $target = "522969993";
 	$data_session = $_SESSION['data'];
-	$getinfo = proccess(1, $data_session['useragent'], 'users/'.$target.'/info/',$data_session['cookies']);
+	$getinfo = proccess(1, $data_session['useragent'], 'users/'.$target.'/info/');
 	$getinfo = json_decode($getinfo[1]);
 	if($_POST['tipe']=='followers'):
 		if(!is_numeric($jumlah))
@@ -49,19 +47,17 @@ while(true){
 	for($i=0;$i<count($listids);$i++):
 			$cross = proccess(1, $data_session['useragent'], 'friendships/create/'.$listids[$i].'/', $data_session['cookies'], hook('{"user_id":"'.$listids[$i].'"}'));
 			$cross = json_decode($cross[1]);
-			print $xx++.'. <b>@'.$data_session['username'].' Follow => '.$listids[$i]." ".$cross->status.PHP_EOL;
+			print $i.'. <b>@'.$data_session['username'].'</b> <font color="green">Sukses Follow => </font><b style="color:gray;">[ @'.$listids[$i].' ]</b><br>';
 			flush();
-     
 	endfor;
-	 sleep(300);
 }
 else
 {
-         $jumlah= "9";
+        $jumlah= "11";
         $_POST['tipe'] = "following";
-        $target = $_SESSION['data']['id'];
+	$target = $_SESSION['data']['id'];
 	$data_session = $_SESSION['data'];
-	$getinfo = proccess(1, $data_session['useragent'], 'users/'.$target.'/info/',$data_session['cookies']);
+	$getinfo = proccess(1, $data_session['useragent'], 'users/'.$target.'/info/');
 	$getinfo = json_decode($getinfo[1]);
 	if($_POST['tipe']=='following'):
 		if(!is_numeric($jumlah))
@@ -74,11 +70,11 @@ else
 	else:
 		if(!is_numeric($jumlah))
 			$limit = 1;
-		elseif($jumlah>($getinfo->user->following_count-1))
-			$limit = $getinfo->user->following_count-1;
+		elseif($jumlah>($getinfo->user->follower_count-1))
+			$limit = $getinfo->user->follower_count-1;
 		else
 			$limit = $jumlah-1;
-		$tipe = 'following';
+		$tipe = 'followers';
 	endif;
 	$c = 0;
 	$listids = array();
@@ -93,16 +89,10 @@ else
 		$c = (isset($req->next_max_id)) ? $req->next_max_id : 0;
 	}while(count($listids)<=$limit);
 	for($i=0;$i<count($listids);$i++):
-			$cross = proccess(1, $data_session['useragent'], 'friendships/create/'.$listids[$i].'/', $data_session['cookies'], hook('{"user_id":"'.$listids[$i].'"}'));
+			$cross = proccess(1, $data_session['useragent'], 'friendships/destroy/'.$listids[$i].'/', $data_session['cookies'], hook('{"user_id":"'.$listids[$i].'"}'));
 			$cross = json_decode($cross[1]);
-			print $xx++.'. <b>@'.$data_session['username'].' Follow => '.$listids[$i]." ".$cross->status.PHP_EOL;
+			print $i.'. <b>@'.$data_session['username'].'</b> <font color="green">Sukses Follow => </font><b style="color:gray;">[ @'.$listids[$i].' ]</b><br>';
 			flush();
-     
 	endfor;
-	 sleep(300);
-}
-	
-	
-	
 }
 ?>

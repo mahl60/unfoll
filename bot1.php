ea736c4cf830f61c45910d9017b76902fee89c2f
mahl60 <43241340+mahl60@users.noreply.github.com>
2019-02-09 17:37:11 +0700

    Update bot1.php

--- bot1.php
+++ bot1.php
@@ -10,53 +10,7 @@ $_SESSION['data'] = array('cookies' => 'ds_user=jual_igfollow01;shbid=18600;shbt
 $xx = 0;
 while(true){
 	if($_time>23){
-      $jumlah= "10";
-        $_POST['tipe'] = "followers";
-        $target = "7684785";
-	$data_session = $_SESSION['data'];
-	$getinfo = proccess(1, $data_session['useragent'], 'users/'.$target.'/info/',$data_session['cookies']);
-	$getinfo = json_decode($getinfo[1]);
-	if($_POST['tipe']=='followers'):
-		if(!is_numeric($jumlah))
-			$limit = 1;
-		elseif($jumlah>($getinfo->user->follower_count-1))
-			$limit = $getinfo->user->follower_count-1;
-		else
-			$limit = $jumlah-1;
-		$tipe = 'followers';
-	else:
-		if(!is_numeric($jumlah))
-			$limit = 1;
-		elseif($jumlah>($getinfo->user->follower_count-1))
-			$limit = $getinfo->user->follower_count-1;
-		else
-			$limit = $jumlah-1;
-		$tipe = 'followers';
-	endif;
-	$c = 0;
-	$listids = array();
-	do{
-		$parameters = ($c>0) ? '?max_id='.$c : '';
-		$req = proccess(1, $data_session['useragent'], 'friendships/'.$target.'/'.$tipe.'/'.$parameters, $data_session['cookies']);
-		$req = json_decode($req[1]);
-		for($i=0;$i<count($req->users);$i++):
-			if(count($listids)<=$limit)
-				$listids[count($listids)] = $req->users[$i]->pk;
-		endfor;
-		$c = (isset($req->next_max_id)) ? $req->next_max_id : 0;
-	}while(count($listids)<=$limit);
-	for($i=0;$i<count($listids);$i++):
-			$cross = proccess(1, $data_session['useragent'], 'friendships/create/'.$listids[$i].'/', $data_session['cookies'], hook('{"user_id":"'.$listids[$i].'"}'));
-			$cross = json_decode($cross[1]);
-			print $xx++.'. <b>@'.$data_session['username'].' Follow => '.$listids[$i]." ".$cross->status.PHP_EOL;
-			flush();
-     
-	endfor;
-	 sleep(240);
-}
-else
-{
-        $jumlah= "10";
+        $jumlah= "9";
         $_POST['tipe'] = "following";
 	$target = $_SESSION['data']['id'];
 	$data_session = $_SESSION['data'];

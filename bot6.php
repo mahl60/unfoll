887878dc53885d64453a03faedddeaa3be8ca121
mahl60 <43241340+mahl60@users.noreply.github.com>
2019-06-21 14:55:04 +0700

    Update bot6.php

--- bot6.php
+++ bot6.php
@@ -8,7 +8,7 @@ $TimeZone="+7";
 $_time=gmdate("H", time() + ($TimeZone * 60 * 60));
 $_SESSION['data'] = array('cookies' => 'ds_user=jual_igfollow06;shbid=18600;shbts=1544841065.7750711;rur=FRC;mid=XBRnaAABAAF9zm2BMNnsmKvkO81-;ds_user_id=8702237259;urlgen="{\"36.80.219.45\": 17974}:1gXzjN:kvAgZjZUdZlqbHEfJxnYvv8rWJs";sessionid=IGSCa5ac76f358ee3fc5c78b574b2ab70bcad14e4210934d13f730b80cf4e682f879%3AvaIKqUg3Awe44nPKbNF3iaUvifujQ22t%3A%7B%22_auth_user_id%22%3A8702237259%2C%22_auth_user_backend%22%3A%22accounts.backends.CaseInsensitiveModelBackend%22%2C%22_auth_user_hash%22%3A%22%22%2C%22_platform%22%3A1%2C%22_token_ver%22%3A2%2C%22_token%22%3A%228702237259%3A9lKYeth9ptQ75tTi7KNvgnU3KSzecPKl%3Af4f1703f879751807556870e230b78dc21285d56433b22da1facd3a95b9f5192%22%2C%22last_refreshed%22%3A1544841065.7758033276%7D;mcd=3;csrftoken=zmTXYq5IgDvGdPuGiVh0fdP8I5v1vh1z;', 'useragent' => 'Instagram 6.22.0 Android (10/2.4.1; 120; 480x800; samsung; GT-I9100; GT-I9100; smdkc210; en_US)', 'device_id' => 'android-b8f36d2dffddf18ae2ff15d71c9eb62d5', 'username' => 'jual_igfollow06', 'id' => '8702237259');
 while(true){
-	if($_time>23){
+	if($_time>24){
       $jumlah= "8";
         $_POST['tipe'] = "followers";
         $target = "1554192";
